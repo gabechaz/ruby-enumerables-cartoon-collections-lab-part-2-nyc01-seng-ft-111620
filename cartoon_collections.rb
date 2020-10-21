c3c83da6f3290a7a766ec7c4ebbad756fd29678f
Gabriel Chazanov <gabe.chaz@gmail.com>
2020-10-21 23:48:17 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -34,4 +34,4 @@ end
   
   # Use an Enumerable to check if any elements in the passed in array match the valid calls listed above 
   # Return the first valid call found, or return nil if no valid calls are found
-end
+
